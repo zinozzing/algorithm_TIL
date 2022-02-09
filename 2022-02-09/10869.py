2645f89919ed75f94c0272806a4daac46cfb5d9f
zinozzing <rlawlsgh1111@naver.com>
2022-02-09 23:35:48 +0900

    TIL step 1

---
+++ 2022-02-09/10869.py
@@ -0,0 +1,8 @@
+# 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+
+A, B = map(int, input().split())
+print(A+B)
+print(A-B)
+print(A*B)
+print(int(A/B))
+print(A%B)
---
+++ 2022-02-09/2588.py
@@ -0,0 +1,9 @@
+# (1)과 (2)위치에 들어갈 세 자리 자연수가 주어질 때 (3), (4), (5), (6)위치에 들어갈 값을 구하는 프로그램을 작성하시오.
+
+A= str(input())
+B= str(input())
+
+print(int(A) * int(B[2]))
+print(int(A) * int(B[1]))
+print(int(A) * int(B[0]))
+print(int(A) * int(B))
